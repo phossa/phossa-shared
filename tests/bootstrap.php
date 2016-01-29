00d95c30c1efb0d1b119056cc13de724e3455100
Hong Zhang <phossa@126.com>
2016-01-29 12:47:57 +0800

    clean up
    clean up

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,3 @@ if (file_exists(dirname($basedir).'/autoload.php')) {
     # package autoloader
     $loader = require $basedir.'/vendor/autoload.php';
 }
-
-/*
-$loader->add('Phossa\\Shared\\', $basedir . '/src/');
-$loader->add('Phossa\\Shared\\', __DIR__ . '/src/');
-*/
